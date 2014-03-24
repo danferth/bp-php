e1c0ad9460fbd16e5887a4418e8841f1f0439e82
danferth <danferth@gmail.com>
2014-03-24 16:13:46 -0700

    fixed issue, turns out the form folder was a cloned gist so had a .git folder included that was hidden and the form folder became a submodule.  This throw me as I never worked with submodules and the containing folder was called modules so confusion and a waste of time insued

---
+++ modules/form/thank-you.php
@@ -0,0 +1,6 @@
+<?php
+$first_name = $_GET['first_name'];
+?>
+// page has query in url with users name so used in thank you message
+<p>Thank you <?php echo $first_name; ?>, for using our form.</p>
+
