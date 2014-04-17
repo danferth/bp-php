76a21339c109f27fc9f37fbe65a6dbb97644694b
danferth <danferth@gmail.com>
2014-04-16 17:31:04 -0700

    edit to __test items

--- __test_view.php
+++ __test_view.php
@@ -5,3 +5,5 @@ include $_SERVER['DOCUMENT_ROOT'].'/__test_data.php';
 ?>
 
 <p>You sir, are on a test page!</p>
+<p><i>__test_class.php</i> is included, and holds the class we are testing</p>
+<p><i>__test_data.php</i> in included second, and contains any data the class may need (9see array)</p>
